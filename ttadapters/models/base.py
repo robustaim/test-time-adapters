c99bb75ead223c81bc7720bd9b7f35f00460b2ac
BREW <81044362+b-re-w@users.noreply.github.com>
2025-05-28 08:14:34 +0900

    Feat: Add RT-DETR

---
+++ ttadapters/models/base.py
@@ -0,0 +1,32 @@
+from torch import nn, load, save
+from os import path, makedirs
+
+
+class BaseModel(nn.Module):
+    model_name = "BaseModel"
+    dataset_name = "Dataset"
+
+    def __init__(self, image_size: int, num_classes: int):
+        super().__init__()
+        self.image_size = image_size
+        self.num_classes = num_classes
+
+    def forward(self, x, *args, **kwargs):
+        raise NotImplementedError("The forward method must be implemented in subclasses.")
+
+    def save(self, save_path: str = path.join(".", "weights"), version: str = "", silence: bool = False):
+        if not path.isdir(save_path):
+            makedirs(save_path)
+        if version:
+            version = f"_{version}"
+        model_id = f"{self.model_name}_{self.dataset_name}{version}"
+        file_name = path.join(save_path, f"{model_id}.pt")
+        save(self.state_dict(), file_name)
+        if not silence: print(f"INFO: Model saved to {file_name}")
+
+    def load(self, version: str = "", weight_path: str = path.join(".", "weights")):
+        if version:
+            version = f"_{version}"
+        model_id = f"{self.model_name}_{self.dataset_name}{version}"
+        file_name = path.join(weight_path, f"{model_id}.pt")
+        self.load_state_dict(load(file_name, map_location="cpu"))
