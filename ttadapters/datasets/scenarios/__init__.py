248f81ec4c7f21ff82673277b5f3f1a332d88420
BREW <81044362+b-re-w@users.noreply.github.com>
2025-10-02 14:06:41 +0900

    Temp

--- ttadapters/datasets/scenarios/__init__.py
+++ ttadapters/datasets/scenarios/__init__.py
@@ -1,29 +1,35 @@
 from torch.utils.data import DataLoader
 from typing import Optional, Callable
+from tqdm.auto import tqdm
 import math
 
 from .. import (
-    SHIFTDiscreteSubsetForObjectDetection
+    SHIFTDiscreteSubsetForObjectDetection,
+    SHIFTContinuousSubsetForObjectDetection,
+    SHIFTContinuous10SubsetForObjectDetection,
+    SHIFTContinuous100SubsetForObjectDetection
 )
 
 
-SubsetType = SHIFTDiscreteSubsetForObjectDetection.SubsetType
+DiscreteSubsetType = SHIFTDiscreteSubsetForObjectDetection.SubsetType
+ContinuousSubsetType = SHIFTContinuousSubsetForObjectDetection.ContinuousSubsetType
 
 
 class SHIFTDiscreteScenario(dict):
     keys = [
-        SubsetType.CLEAR_DAYTIME,  # same as NORMAL
-        SubsetType.CLEAR_NIGHT,
-        SubsetType.CLEAR_DAWN,
-        SubsetType.CLOUDY_DAYTIME,
-        SubsetType.OVERCAST_DAYTIME,
-        SubsetType.FOGGY_DAYTIME,
-        SubsetType.RAINY_DAYTIME,
+        DiscreteSubsetType.CLEAR_DAYTIME,  # same as NORMAL
+        DiscreteSubsetType.CLEAR_NIGHT,
+        DiscreteSubsetType.CLEAR_DAWN,
+        DiscreteSubsetType.CLOUDY_DAYTIME,
+        DiscreteSubsetType.OVERCAST_DAYTIME,
+        DiscreteSubsetType.FOGGY_DAYTIME,
+        DiscreteSubsetType.RAINY_DAYTIME,
     ]
+    description = "SHIFT Discrete Scenario"
 
     def __init__(
         self, root: str, force_download: bool = False,
-        train: bool = True, valid: bool = False, exclude_list: Optional[SubsetType] = None,
+        train: bool = True, valid: bool = False, exclude_list: Optional[DiscreteSubsetType] = None,
         transform: Optional[Callable] = None, target_transform: Optional[Callable] = None, transforms: Optional[Callable] = None
     ):
         super().__init__(self)
@@ -44,10 +50,10 @@ class SHIFTDiscreteScenario(dict):
 
     def play(self, script: Callable, **kwargs):
         result = {}
-        for key, dataset in self.items():
+        for key, dataset in tqdm(self.items(), desc=self.description):
             loader = DataLoader(dataset, **self._play_config)
             loader_len = math.ceil(len(dataset)/loader.batch_size)
-            bench = script(loader, loader_len, **kwargs)
+            bench = script(key.value, loader, loader_len, **kwargs)
             result[key] = bench
 
         result_list = list(result.values())
@@ -58,3 +64,7 @@ class SHIFTDiscreteScenario(dict):
 
         del self._play_config
         return result, result_mean
+
+
+class SHIFTContinuousScenario(SHIFTDiscreteScenario):
+    pass
