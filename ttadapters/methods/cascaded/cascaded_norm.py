34676c730d942c825878ac3ee7ffc4781c37c7ee
BREW <81044362+b-re-w@users.noreply.github.com>
2026-01-20 02:44:31 +0900

    Revert "Fix: Typo"
    This reverts commit 709a819d12f8b7e332a680ed3632385b24abfbf7.

--- ttadapters/methods/cascaded/cascaded_norm.py
+++ ttadapters/methods/cascaded/cascaded_norm.py
@@ -107,7 +107,7 @@ class DifferentiableHistogramStretcher(nn.Module):
 
     def __init__(self, temperature: float = 0.01):
         super().__init__()
-        self.base_temperature = temperature
+        self.temperature = temperature
 
     def soft_percentile_batch(self, x: torch.Tensor, p: torch.Tensor) -> torch.Tensor:
         """
