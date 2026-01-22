11ac9d19f1b265704eb97d9075f131cd0387cc9e
BREW <81044362+b-re-w@users.noreply.github.com>
2026-01-23 05:15:45 +0900

    Feat: Tighter Range

--- ttadapters/methods/cascaded/cascaded_norm_v3.py
+++ ttadapters/methods/cascaded/cascaded_norm_v3.py
@@ -110,7 +110,7 @@ class GammaTransform(nn.Module):
 
     def forward(self, img):
         """Get constrained parameters and apply transform."""
-        noise_floor = self.noise_floor.clamp(min=0.0, max=48.0)
+        noise_floor = self.noise_floor.clamp(min=0.0, max=20.0)
         gamma = self.gamma.clamp(min=0.1, max=5.0)
 
         transformed = self.stretcher(img, noise_floor, self.saturation_limit, gamma)
