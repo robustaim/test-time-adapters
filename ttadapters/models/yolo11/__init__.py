b91453b1adebe7d2e0fac95c94b3689310abed9a
BREW <81044362+b-re-w@users.noreply.github.com>
2026-02-05 15:59:59 +0900

    Fix: Aliasing

--- ttadapters/models/yolo11/__init__.py
+++ ttadapters/models/yolo11/__init__.py
@@ -1,1 +1,6 @@
+import sys
+from . import modeling_yolo11 as __modeling_yolo11
 from .modeling_yolo11 import YOLO11ForObjectDetection
+
+
+sys.modules['ttadapters.models.yolo11.modelings'] = __modeling_yolo11
