698104c22b60ba873b244917c20c0fb6e318f987
BREW <81044362+b-re-w@users.noreply.github.com>
2026-03-01 01:46:50 +0900

    Revert "Fix: _LazyAutoMapping({}, {})"
    This reverts commit 584b19cb376ff690202ea67d89af70be5c4d3af7.

--- ttadapters/methods/base.py
+++ ttadapters/methods/base.py
@@ -9,7 +9,6 @@ import torch
 from torch import nn, optim
 from muon import Muon, MuonWithAuxAdam
 from transformers import PretrainedConfig, PreTrainedModel
-from transformers.models.auto.modeling_auto import _LazyAutoMapping
 from transformers.models.auto.auto_factory import _BaseAutoModelClass, auto_class_update
 
 from ..models.base import BaseModel, ModelProvider, DataPreparation
@@ -271,12 +270,12 @@ class AdaptationEngine(BaseModel, OnlineMixin, PreTrainedModel):
 
 class AutoAdaptationEngine(_BaseAutoModelClass):
     """Auto class for Test-time Adaptation"""
-    _model_mapping = _LazyAutoMapping({}, {})
+    _model_mapping = {}
 
 
 class AutoAdaptationEngineForObjectDetection(_BaseAutoModelClass):
     """Auto class for TTA-OD"""
-    _model_mapping = _LazyAutoMapping({}, {})
+    _model_mapping = {}
 
 
 AutoAdaptationEngine = auto_class_update(AutoAdaptationEngine, head_doc="test time adaptation")
